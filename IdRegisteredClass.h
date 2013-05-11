f0bb213ca2a471a580968cdc768ef138c8faf33c
Franciszek Boehlke <franio.kropka.b@gmail.com>
2013-05-11 15:42:37 +0200

    IdRegisteredClass

---
+++ IdRegisteredClass.h
@@ -0,0 +1,73 @@
+#ifndef IDREGISTEREDCLASS_H
+#define IDREGISTEREDCLASS_H
+
+#include <QHash>
+
+template <uint IdClass>
+class IdRegisteredClass
+{
+public:
+    typedef uint IdType;
+    IdRegisteredClass()
+    {
+        do
+            idVal = nextid++;
+        while (hashmap.contains(idVal));
+        hashmap.insert(idVal, this);
+    }
+
+    explicit IdRegisteredClass(IdType id)
+    {
+        //byc moze powinno rzucac wyjatkiem, jezli id zajete
+        idVal = id;
+        while (hashmap.contains(idVal))
+            idVal = nextid++;
+        hashmap.insert(idVal, this);
+    }
+
+    virtual ~IdRegisteredClass()
+    {
+        hashmap.remove(idVal);
+    }
+
+    IdType id() const
+    {
+        return idVal;
+    }
+
+    static IdRegisteredClass* getById(IdType id)
+    {
+        return hashmap.value(id);
+    }
+
+    template <typename classname>
+    static classname getByIdAs(IdType id)
+    {
+        //cast referencji, zeby rzucal wyjatkiem przy bledzie
+        return &dynamic_cast<classname>(*(hashmap.value(id)));
+    }
+private:
+    static QHash<IdType, IdRegisteredClass*> hashmap;
+    static IdType nextid;
+    IdType idVal;
+};
+
+//zmienne statyczne
+
+template<uint IdClass>
+QHash<typename IdRegisteredClass<IdClass>::IdType, IdRegisteredClass<IdClass>*> IdRegisteredClass<IdClass>::hashmap;
+
+template<uint IdClass>
+uint IdRegisteredClass<IdClass>::nextid = 0;
+
+typedef IdRegisteredClass<0> ModelIdClass;
+typedef ModelIdClass::IdType ModelId;
+
+typedef IdRegisteredClass<1> SystemViewIdClass;
+typedef SystemViewIdClass::IdType SystemViewId;
+
+typedef IdRegisteredClass<2> VoiceListIdClass;
+typedef VoiceListIdClass::IdType VoiceListId;
+
+
+#endif // IDREGISTEREDCLASS_H
