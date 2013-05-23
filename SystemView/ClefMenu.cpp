7fd2f374e30951555a976e4c0d104d97eebe0504
Franciszek Boehlke <franio.kropka.b@gmail.com>
2013-05-23 19:54:31 +0200

    ClefMenu actions

--- SystemView/ClefMenu.cpp
+++ SystemView/ClefMenu.cpp
@@ -3,4 +3,35 @@
 ClefMenu::ClefMenu(QObject *parent) :
     CuteMenu(parent)
 {
+    treble = addAction(tr('Treble clef'));
+    bass = addAction(tr('Bass clef'));
+    alto = addAction(tr('Alto clef'));
+    neutral = addAction(tr('Neutral clef'));
+    other = addAction(tr('Other clef'));
 }
+
+QAction* ClefMenu::trebleAction() const
+{
+    return treble;
+}
+
+QAction* ClefMenu::bassAction() const
+{
+    return bass;
+}
+
+QAction* ClefMenu::altoAction() const
+{
+    return alto;
+}
+
+QAction* ClefMenu::neutralAction() const
+{
+    return neutral;
+}
+
+QAction* ClefMenu::otherAction() const
+{
+    return other;
+}
+
--- SystemView/ClefMenu.h
+++ SystemView/ClefMenu.h
@@ -2,13 +2,24 @@
 #define CLEFMENU_H
 
 #include "CuteMenu.h"
+#include <QAction>
 
 class ClefMenu : public CuteMenu
 {
     Q_OBJECT
+    QAction* treble;
+    QAction* bass;
+    QAction* alto;
+    QAction* neutral;
+    QAction* other;
 public:
     explicit ClefMenu(QObject *parent = 0);
-    
+
+    QAction* trebleAction() const;
+    QAction* bassAction() const;
+    QAction* altoAction() const;
+    QAction* neutralAction() const;
+    QAction* otherAction() const;
 signals:
     
 public slots:
