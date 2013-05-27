270f8516935c6a2957d0106fb9aff766fa3ee42b
korzenek <korzenek@gmail.com>
2013-05-27 13:27:51 +0200

    niby zakodzone, ale nie mam jak sprawdzic czy dziala

--- SystemImageInfo.cpp
+++ SystemImageInfo.cpp
@@ -8,12 +8,28 @@ SystemImageInfo::SystemImageInfo()
 
 SystemImageInfo::SystemImageInfo(const QString &fileName)
     : fileNameVal(fileName)
+    Poppler::Document *doc = Poppler::Document::load(fileName);
+    QImage qimage = doc->page(currentPage)->renderToImage(
+                            scaleFactor * physicalDpiX(),
+                            scaleFactor * physicalDpiY());
+    image = QPixmap::fromImage(qimage);
 {
+}
+
+bool SystemImageInfo::isNull() const
+{
+    return (fileNameVal==NULL)&&(QPixmap==NULL);
 }
 
 QString SystemImageInfo::fileName() const
 {
     return fileNameVal;
+}
+
+voif SystemImageInfo::remember()
+{
+    remember = True;
+    image = image.transformed();
 }
 
 
@@ -25,4 +41,11 @@ void SystemImageInfo::setFileName(const QString &fileName)
 QPixmap SystemImageInfo::pixmap() const
 {
     return QPixmap(fileName());
+}
+
+QPixmap SystemImageInfo::transformed() const
+{   
+    QPixmap qmap = SystemImageInfo.pixmap().copy(frame);
+    return qmap.transformed(&transformation, 
+        Qt::TransformationModemode=Qt::FastTransformation);
 }
--- SystemImageInfo.h
+++ SystemImageInfo.h
@@ -8,14 +8,21 @@ class QPixmap;
 class SystemImageInfo
 {
     QString fileNameVal;
+    int pageNumber;
+    QTransform transformation;
+    QRect frame;
+    QPixmap image;
+    bool remember;
 public:
     SystemImageInfo();
     SystemImageInfo(const QString& fileName);
 
+    bool isNull() const;
     QString fileName() const;
     void setFileName(const QString& fileName);
-
     QPixmap pixmap() const;
+    QPixmap transformed() const;
+    void remember();
 };
 
 #endif // SYSTEMIMAGEINFO_H
