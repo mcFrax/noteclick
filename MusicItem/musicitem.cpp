540e8d0abd26d3c0eafa6377014fb1fb4a565913
Wojciech Donderowicz <jotek7@gmail.com>
2013-05-28 03:40:19 +0200

    music item branch

---
+++ MusicItem/musicitem.cpp
@@ -0,0 +1,51 @@
+#include "musicitem.h"
+
+
+MusicItem::MusicItem(QGraphicsItem *parent) :
+    QGraphicsPixmapItem()
+{
+    setFlag(QGraphicsItem::ItemIsMovable, true);
+    setFlag(QGraphicsItem::ItemIsSelectable, true);
+    currentState = -1;
+    generateStates();
+}
+
+QPixmap MusicItem::changeColor(QPixmap &pixmap, QColor c)
+{
+    QImage image = pixmap.toImage();
+    QRgb old;
+    int width = pixmap.width();
+    int height = pixmap.height();
+    for (int i = 0; i < width; ++i)
+    {
+        for (int j = 0; j < height; ++j)
+        {
+            old = image.pixel(i, j);
+            if (qAlpha(old) == 255)
+                image.setPixel(i, j, c.rgb());
+        }
+    }
+
+    return pixmap.fromImage(image);
+}
+
+void MusicItem::addState(int number, QPixmap &pixmap)
+{
+    states[number] = pixmap;
+}
+
+void MusicItem::setState(int number)
+{
+    currentState = number;
+    setPixmap(states[number]);
+}
+
+int MusicItem::getState()
+{
+   return currentState;
+}
+
+void MusicItem::generateStates()
+{
+
+}
